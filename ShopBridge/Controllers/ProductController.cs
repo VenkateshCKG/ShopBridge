376a0abb9d89e8de6777678b8e5024bf1edb9c96
Venkatesh C <venkatc097@gmail.com>
2021-04-16 17:27:03 +0530

    Add UI

--- ShopBridge/Controllers/ProductController.cs
+++ ShopBridge/Controllers/ProductController.cs
@@ -11,7 +11,9 @@ using System.Web.Http;
 
 namespace ShopBridge.Controllers
 {
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     public class ProductController : ApiController
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
     {
         DataBaseContext db = new DataBaseContext();
         [HttpGet]
