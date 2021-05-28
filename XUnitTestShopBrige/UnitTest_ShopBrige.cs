e2a57b00dde7daf6cfaafa2c11c6cea32e4850df
Venkatesh C <venkatc097@gmail.com>
2021-05-28 09:01:55 +0530

    bulid

--- XUnitTestShopBrige/UnitTest_ShopBrige.cs
+++ XUnitTestShopBrige/UnitTest_ShopBrige.cs
@@ -6,16 +6,22 @@ using System.Net.Http;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using System.Net;
+using ShopBridge.Services.Repository;
+using ShopBridge.Models;
 
 namespace XUnitTestShopBrige
 {
     public class UnitTest_ShopBrige
     {
-        ProductController product = new ProductController();
+        readonly ProductRepository productRepository;
+        public UnitTest_ShopBrige()
+        {
+            productRepository = new ProductRepository(new DataBaseContext());
+        }
         [Fact]
         public void Get_Allproduct()
         {
-            var pro = product.GetProducts();
+            var pro = productRepository.GetAllProducts();
             Assert.Equal(pro, pro);
         }
     }
